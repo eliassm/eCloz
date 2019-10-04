e490aa54a72c2eb3d818fa14d4a13ae9b6415923
flashdebugger <flashdebugger0330@gmail.com>
2019-10-04 15:16:43 +1000

    react eCommerce web app initial commit

---
+++ src/components/BaseLayout/components/Footer/index.js
@@ -0,0 +1,12 @@
+//Dependencies
+import React from 'react';
+//Internals
+import './index.css';
+
+const Footer = () => (
+  <div className="footer">
+    <p>© 2016 Copyright <a href="https://github.com/seniordev-rt96" target="_blank">seniordev-rt96</a></p>
+  </div>
+)
+
+export default Footer;
